69edb6897994b428b52597dc68532ee83e5098fb
ivan-po-mars <ivan.po.mars@gmail.com>
2021-11-05 20:23:45 +0200

    homework2

--- src/com/homework2/Main.java
+++ src/com/homework2/Main.java
@@ -2,6 +2,21 @@ package com.homework2;
 
 public class Main {
     public static void main(String[] args) {
+        /*
+        Заповнити масив тільки парними числами
+        Заповнити масив рандомними числами , почитатйте про класс Random;
+        Вивести середнє значення масиву
+        Вивести найменше значення в масиві
+        Вивести найбільше значення масиву
+        Вивести значення парних індексів масиву
+        Вивести перших 20 символів з таблиці ASCI
+         */
 
+        int [] arr = {1, 2, 3, 4, 5};
+
+        int [] emptyArr = new int[10];
+
+        System.out.println(arr[2]);
+        System.out.println(emptyArr.length);
     }
 }
